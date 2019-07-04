31e1bddc969e683db6faf953088b419be9f2e293
Joshua Duscha <jduscha@hh.rexx-systems.rx>
2019-07-04 14:13:49 +0200

    Initialize

---
+++ contentScript.js
@@ -0,0 +1,17 @@
+chrome.storage.onChanged.addListener(function (changes, namespace) {
+    for (let key in changes) {
+        let storageChange = changes[key];
+        console.log('Storage key "%s" in namespace "%s" changed. ' +
+            'Old value was "%s", new value is "%s".',
+            key,
+            namespace,
+            storageChange.oldValue,
+            storageChange.newValue);
+    }
+});
+
+init();
+
+function init() {
+    let options = new Options();
+}
