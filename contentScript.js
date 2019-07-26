62afd4552b224e7447e5ed329651b3bf746e0428
Joshua Duscha <jduscha@hh.rexx-systems.rx>
2019-07-26 08:36:52 +0200

    Create Options

--- contentScript.js
+++ contentScript.js
@@ -10,8 +10,64 @@ chrome.storage.onChanged.addListener(function (changes, namespace) {
     }
 });
 
+class Options {
+    _hours;
+    _breaks;
+    _notifications;
+
+    get hours() {
+        return this._hours;
+    }
+    get breaks() {
+        return this._breaks;
+    }
+    get notifications() {
+        return this._notifications;
+    }
+    constructor(hours, breaks, notifications) {
+        this._hours = hours;
+        this._breaks = breaks;
+        this._notifications = notifications;
+    }
+}
+
 init();
 
 function init() {
-    let options = new Options();
+    loadOptions(main);
+}
+
+function loadOptions(callback) {
+    chrome.storage.sync.get(['hours', 'breaks', 'notifications'], function (result) {
+        if (Object.keys(result).length === 0) {
+            result.hours = [6, 8, 9, 10];
+            chrome.storage.sync.set({hours: result.hours}, function () {});
+        }
+
+        if (Object.keys(result).length === 0) {
+            result.breaks = {
+                '6': 30,
+                '9': 45
+            };
+            chrome.storage.sync.set({breaks: result.breaks}, function () {});
+        }
+
+        if (Object.keys(result).length === 0) {
+            result.notifications = {
+                beforeBreak: [15],
+                atHour: [4]
+            };
+            chrome.storage.sync.set({notifications: result.notifications}, function () {});
+        }
+
+        let options = new Options(result.hours, result.breaks, result.notifications);
+
+        callback(options);
+    });
+
+
+}
+
+function main(options) {
+
 }
