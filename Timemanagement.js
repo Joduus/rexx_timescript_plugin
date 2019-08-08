4792cd1183023ee4e66bc568e34019daf1b31bf2
Joshua Duscha <jduscha@hh.rexx-systems.rx>
2019-08-08 17:08:39 +0200

    Add timeEntries

---
+++ Timemanagement.js
@@ -0,0 +1,59 @@
+class TimeManagement {
+    /*
+    class Options
+     */
+    _options;
+
+    /*
+    {
+        {
+            datetime,
+            state(kommen, gehen)
+        },
+        {
+            datetime,
+            state
+        }
+    }
+     */
+    _timeEntries = {};
+
+    _menuBarHtml =
+        '<li class="navbar_txt">' +
+        '   <a id="nav_infotxt_worktime_string">Arbeitszeit:</a>' +
+        '</li>';
+
+    constructor(options) {
+        this._options = options;
+
+        this.getTodayTimeEntries();
+
+        this.appendHtml();
+    }
+
+    getTodayTimeEntries() {
+        let todayTimeEntriesContainer = document.querySelector('.grid_table .grid_row_highlight_today');
+        let timeEntries = todayTimeEntriesContainer.children[2].children[0];
+        let stateEntries = todayTimeEntriesContainer.children[1].children[0];
+
+        let today = new Date();
+        let todayDate = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+        for (let i = 0; timeEntries.children.length > i; i++) {
+            this._timeEntries[i] = {
+                time: new Date(todayDate + ' ' + timeEntries.children[i].querySelector('span').textContent),
+                state: stateEntries.children[i].querySelector('a').text
+            };
+        }
+        console.log(this._timeEntries);
+    }
+
+    appendHtml() {
+        let menuBar = parent.document.getElementById('list_actions');
+
+        menuBar.insertAdjacentHTML("beforeend", this._menuBarHtml);
+    }
+
+    run() {
+        // Run timemanagment
+    }
+}
--- contentScript.js
+++ contentScript.js
@@ -10,84 +10,13 @@ chrome.storage.onChanged.addListener(function (changes, namespace) {
     }
 });
 
-class Options {
-    /*
-    [
-        tracked hours
-    ]
-     */
-    _hours;
-    /*
-    {
-        key: afterHours,
-        value: how much break (min)
-    }
-     */
-    _breaks;
-    /*
-    {
-        beforeBreak: [
-            min before autopause
-        ],
-        atHour: [
-            notificate on this time
-        ]
-    }
-     */
-    _notifications;
-
-    get hours() {
-        return this._hours;
-    }
-    get breaks() {
-        return this._breaks;
-    }
-    get notifications() {
-        return this._notifications;
-    }
-    constructor(hours, breaks, notifications) {
-        this._hours = hours;
-        this._breaks = breaks;
-        this._notifications = notifications;
-    }
-}
-
 init();
 
 function init() {
     loadOptions(main);
 }
 
-function loadOptions(callback) {
-    chrome.storage.sync.get(['hours', 'breaks', 'notifications'], function (result) {
-        if (Object.keys(result.hours).length === 0) {
-            result.hours = [6, 8, 9, 10];
-            chrome.storage.sync.set({hours: result.hours}, function () {});
-        }
-
-        if (Object.keys(result.breaks).length === 0) {
-            result.breaks = {
-                '6': 30,
-                '9': 45
-            };
-            chrome.storage.sync.set({breaks: result.breaks}, function () {});
-        }
-
-        if (Object.keys(result.notifications).length === 0) {
-            result.notifications = {
-                beforeBreak: [15],
-                atHour: [4]
-            };
-            chrome.storage.sync.set({notifications: result.notifications}, function () {});
-        }
-
-        let options = new Options(result.hours, result.breaks, result.notifications);
-        callback(options);
-    });
-
-
-}
-
 function main(options) {
-
+    let timeManagement = new TimeManagement(options);
+    timeManagement.run();
 }
