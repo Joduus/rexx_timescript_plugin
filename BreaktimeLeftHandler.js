8fb8f32745951ae182b09d5e932d9742e55977f5
Joshua Duscha <jduscha@hh.rexx-systems.rx>
2019-12-05 16:06:35 +0100

    Fix BreaktimeLeftHandler again

--- BreaktimeLeftHandler.js
+++ BreaktimeLeftHandler.js
@@ -3,8 +3,6 @@ class BreaktimeLeftHandler extends TimeHandler {
         breaktimeLeft: 'nav_infotxt_breaktimeLeft'
     };
 
-    _allReached = false;
-
     _breakTimes = [ // Set by the options only now hardcoded
         {
             'afterTime': 21600, // 6 Hours
@@ -16,10 +14,10 @@ class BreaktimeLeftHandler extends TimeHandler {
         }
     ];
 
-    _worktime;
+    _currentWorktime;
+    _currentBreaktime;
 
-    _breaktime;
-
+    _breaktimeDifference;
     _breaktimeReached = false;
     _isPerfectInTime = false;
 
@@ -29,6 +27,10 @@ class BreaktimeLeftHandler extends TimeHandler {
 
     run(currentTime) {
         super.run(currentTime);
+
+        if (this._breakTimes.length === 0) {
+            return;
+        }
 
         this.updateBreakTimeLeft();
 
@@ -44,9 +46,13 @@ class BreaktimeLeftHandler extends TimeHandler {
 </li>
 `;
 
+    get currentBreaktime() {
+        return this._breaktimeDifference;
+    }
+
     updateBreakTimeLeft() {
-        this._worktime = this._handler.worktimeHandler.worktime;
-        this._breaktime = this._handler.breaktimeHandler.breaktime;
+        this._currentWorktime = this._handler.worktimeHandler.worktime;
+        this._currentBreaktime = this._handler.breaktimeHandler.breaktime;
 
         this.resetTimeObject();
 
@@ -55,20 +61,14 @@ class BreaktimeLeftHandler extends TimeHandler {
         for (let i = 0; i < this._breakTimes.length; i++) {
             howLongTotal += this._breakTimes[i].howLong;
 
-            if (this._worktime < this._breakTimes[i].afterTime) {
+            if (this._currentWorktime < this._breakTimes[i].afterTime) {
                 firstNotReachedTime = this._breakTimes[i];
                 break;
             }
         }
 
-        if (firstNotReachedTime === -1) {
-            this._allReached = true;
-            return;
-        } else {
-            this._allReached = false;
-        }
-
-        let timeDifference = TimeCalculator.getDifference(this._breaktime, howLongTotal);
+        let timeDifference = TimeCalculator.getDifference(this._currentBreaktime, howLongTotal);
+        this._breaktimeDifference = timeDifference;
 
         if (timeDifference < 1) {
             this._breaktimeReached = true;
@@ -83,25 +83,19 @@ class BreaktimeLeftHandler extends TimeHandler {
         let element = parent.document.getElementById(this._domIds.breaktimeLeft);
         let text = '';
 
-        if (!this._allReached) {
-            if (this._breaktimeReached) {
-                if (!this._isPerfectInTime) {
-                    text += '+';
-                }
-            } else {
-                text += '-';
-            }
-
-            text +=
-                TimeCalculator.toDoubleDigit(this._time.hours)
-                + ':' +
-                TimeCalculator.toDoubleDigit(this._time.minutes);
-
-            if (/*this._options.worktimeLeft.showSeconds*/true) {
-                text += ':' + TimeCalculator.toDoubleDigit(this._time.seconds);
+        if (this._breaktimeReached) {
+            if (!this._isPerfectInTime) {
+                text += '+';
             }
         } else {
-            text = 'Kein Zeitstempel übrig';
+            text += '-';
+        }
+        text +=
+            TimeCalculator.toDoubleDigit(this._time.hours)
+            + ':' +
+            TimeCalculator.toDoubleDigit(this._time.minutes);
+        if (/*this._options.worktimeLeft.showSeconds*/true) {
+            text += ':' + TimeCalculator.toDoubleDigit(this._time.seconds);
         }
 
         element.textContent = text;
