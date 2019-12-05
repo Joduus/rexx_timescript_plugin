3b44aa123db2d6320f5e70eef70710beab6ce044
Joshua Duscha <jduscha@hh.rexx-systems.rx>
2019-12-05 12:32:47 +0100

    Remove declarative content

--- background.js
+++ background.js
@@ -15,29 +15,4 @@
  */
 
 chrome.runtime.onInstalled.addListener(function() {
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-        chrome.declarativeContent.onPageChanged.addRules([{
-            conditions: [
-                new chrome.declarativeContent.PageStateMatcher({
-                    pageUrl: {
-                        hostContains: 'hrbox',
-                        pathContains: 'calendar.php'
-                    },
-                })
-            ],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
-        }])
-    });
-
-    chrome.storage.onChanged.addListener(function (changes, namespace) {
-        for (let key in changes) {
-            let storageChange = changes[key];
-            console.log('Storage key "%s" in namespace "%s" changed. ' +
-                'Old value was "%s", new value is "%s".',
-                key,
-                namespace,
-                storageChange.oldValue,
-                storageChange.newValue);
-        }
-    });
 });
